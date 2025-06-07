150d4ff91f055b2edcca9a6c95207de68232801b
jdluu <j2013agl@gmail.com>
2025-06-07 03:19:21 -0700

    check error flag on exit

--- CC1  - Building WC/ccwc.c
+++ CC1  - Building WC/ccwc.c
@@ -7,9 +7,12 @@ int main(int argc, char*argv[])
 {
   // Create int var for each flag
   int c_flag, l_flag, w_flag, m_flag, err_flag;
-  int byte_count, line_count;
+  // Counter variables
+  int byte_count, line_count, word_count, char_count;
+  
+  char *filename = NULL;
+  FILE *file_ptr = NULL;
 
- 
   // this uses the optstring syntax
   while((c = getopt(argc, argc, ":clwm")) != -1) {
     switch(c) {
@@ -30,8 +33,16 @@ int main(int argc, char*argv[])
         err_flag = 1;
         // optopt is a global integer variable in unistd.h which contains the unknown char
         fprintf(stderr, "Unrecognized option: -%c\n", optopt);
+        break;
       default:
         break;
     }
   }
+  // Exit without any output when err_flag raised
+  if (err_flag) {
+    fprintf(stderr, "Unknown flag provided! Exiting!");
+  }
+
+
+
 }
